62ae8b8b46f958ed78267ba69b724c169e3648ca
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-12-06 16:10:05 +0200

    add GlobalStyles, theme, fonts

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './styles/GlobalStyles';
+import theme from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/car-rental-services">
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+        <GlobalStyles />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+  colors: {
+    background: {
+      primary: '#FFFFFF',
+      secondary: '#F7F7FB',
+    },
+
+    text: {
+      primary: 'rgba(18, 20, 23,0.5)',
+      accent: '#121417',
+      gray: '#8A8A89',
+    },
+
+    accent: {
+      primary: '#3470FF',
+      secondary: '#0B44CD',
+    },
+  },
+
+  fonts: {
+    main: 'Manrope, sans-serif',
+    secondary: 'Montserrat, sans-serif',
+  },
+};
+
+export default theme;

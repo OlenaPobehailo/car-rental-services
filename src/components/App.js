99f5dfe1cae3e34e04fe2fa5c18f84d41e56b83e
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-12-06 14:42:38 +0200

    add  routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import PageNotFound from '../pages/PageNotFound';
+import SharedLayout from './SharedLayout/SharedLayout';
+import HomePage from '../pages/HomePage';
+import CatalogPage from '../pages/CatalogPage';
+import FavoritesPage from '../pages/FavoritesPage';
 
 function App() {
   return (
-    <div>
-    start
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+      </Route>
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 

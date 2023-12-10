9e4806548cdee51cb0740633034620e868073584
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2023-12-10 21:26:26 +0200

    update NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,6 +1,24 @@
+import { Link } from "react-router-dom";
+import {
+  NotFoundImage,
+  NotFoundMessage,
+  NotFoundTitle,
+  NotFoundWrapper,
+  StyledLink,
+} from "./NotFoundPage.styled";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <NotFoundWrapper>
+      <NotFoundTitle>404 Not Found</NotFoundTitle>
+      <NotFoundMessage>
+        Unfortunately, the page you are looking for most likely rented our car
+        and left. Do you want to see it?
+      </NotFoundMessage>
+
+      <StyledLink to="/">Rent a car.</StyledLink>
+    </NotFoundWrapper>
+  );
 };
 
 export default NotFoundPage;
---
+++ src/pages/NotFoundPage.styled.js
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import theme from "styles/theme";
+
+export const NotFoundWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const NotFoundTitle = styled.h1`
+  font-size: 36px;
+  color: #ff5733;
+  margin-bottom: 20px;
+
+  color: ${theme.colors.accent.primary};
+`;
+
+export const NotFoundMessage = styled.p`
+  font-size: 18px;
+  color: ${theme.colors.accent.secondary};
+  margin-bottom: 40px;
+`;
+
+export const StyledLink = styled(Link)`
+  color: ${theme.colors.accent.secondary};
+
+  font-size: 36px;
+  font-weight: 900;
+
+  &:hover {
+    color: ${theme.colors.accent.primary};
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+
+  }
+`;
